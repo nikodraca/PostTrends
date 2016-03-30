d8106d2d5f967507c9935d6d3dd13500fe305d8b
Niko Draca <nikola.draca@live.ca>
2016-03-30 13:04:53 -0400

    remove global vars

--- app.py
+++ app.py
@@ -8,202 +8,154 @@ import requests
 import json 
 import time
 import math
- 
+
 app = Flask(__name__)
 
-followers = None
-follows = None
-pd = None
-lc = None
-cc = None
-df = None
-hm = None
-fc = None
-nodupfc = None
-fcc = None
-avg_likes = None
-posts = None
-followers = None
-follows = None
-max_heightJS = None
-max_height2JS = None
-ratio = None
-moncount = None
-tuecount = None
-wedcount = None
-thucount = None
-fricount = None
-satcount = None
-suncount = None
-hmc = None
-htgs = None
-htgsfinal = None
-hdup1 = None
-hdup2 = None
-htgscount = None
-logt = None
-hashtagerrormsg = None
-zeroerrormsg = None
-usererrormessage = None
-nodupbff = None
-max_heightfilterJS = None
-bffcount = None
-averagetagcount = None
-geolat = None
-geolong = None
-geolatlen = None
-geoname = None
+
+class allVars():
+	followers = None
+	follows = None
+	pd = None
+	lc = None
+	cc = None
+	df = None
+	hm = None
+	fc = None
+	nodupfc = None
+	fcc = None
+	avg_likes = None
+	posts = None
+	max_heightJS = None
+	max_height2JS = None
+	ratio = None
+	moncount = None
+	tuecount = None
+	wedcount = None
+	thucount = None
+	fricount = None
+	satcount = None
+	suncount = None
+	hmc = None
+	htgs = None
+	htgsfinal = None
+	hdup1 = None
+	hdup2 = None
+	htgscount = None
+	logt = None
+	hashtagerrormsg = ""
+	zeroerrormsg = ""
+	usererrormessage = ""
+	nodupbff = None
+	max_heightfilterJS = None
+	bffcount = None
+	averagetagcount = None
+	geolat = None
+	geolong = None
+	geolatlen = None
+	geoname = None
 
 
 @app.route('/')
 def home():
 	return render_template('index.html')
 
-@app.route('/purchase')
-def purchase():
-	return render_template('purchase.html')
-
-
 @app.route("/")
 def getdata(userinput):
 
-	global followers
-	global follows 
-	global pd
-	global lc
-	global cc
-	global df
-	global hm
-	global hmc
-	global avg_likes
-	global posts
-	global followers
-	global follows
-	global max_heightJS
-	global max_height2JS
-	global ratio
-	global moncount
-	global tuecount
-	global wedcount
-	global thucount
-	global fricount
-	global satcount
-	global suncount
-	global htgs
-	global htgsfinal
-	global hdup1
-	global hdup2
-	global htgscount
-	global logt
-	global hashtagerrormsg
-	global zeroerrormsg
-	global usererrormessage
-	global fc
-	global nodupfc
-	global fcc
-	global nodupbff
-	global max_heightfilterJS
-	global bffcount
-	global averagetagcount
-	global geolat 
-	global geolong
-	global geolatlen
-	global geoname
-
-
-	# Enter your user name
-	# userinput = raw_input("Enter a user name: ")
-
 	# Convert username to ID
-	u = requests.get("https://api.instagram.com/v1/users/search?q="+userinput+"/&access_token=")
-	u.text
-
-
-	udata = json.loads(u.text)
+	user_info = requests.get("https://api.instagram.com/v1/users/search?q="+userinput+"/&access_token=ACCESS_TOKEN")
+	user_info.text
+	udata = json.loads(user_info.text)
 
 	try:
-		userid = (udata['data'][0]['id'])
+		user_id = (udata['data'][0]['id'])
 		usererrormessage = ''
 
-
-
 	except IndexError:
-		userid = '25025320'
-		usererrormessage = " (User not found, here's Instagram instead!)"
-
-	# Get account info
-	a = requests.get('https://api.instagram.com/v1/users/'+userid+'/media/recent/?access_token=&count=250')
-	r = requests.get('https://api.instagram.com/v1/users/'+userid+'/?access_token=')
-	r.text
-	a.text
-
-	adata = json.loads(a.text)
-	data = json.loads(r.text)
+		user_id = '25025320'
+		usererrormessage = " User not found, here's Instagram instead!"
 
 
-	n = 20
-	sum = 0
 
-	# Finding likes counts
+	# Get media info
+	media = requests.get('https://api.instagram.com/v1/users/'+user_id+'/media/recent/?access_token=ACCESS_TOKEN&count=250')
+	media_data = json.loads(media.text)
 
-	for x in range(0,n):
+	# Get profile info
+	profile = requests.get('https://api.instagram.com/v1/users/'+user_id+'/?access_token=ACCESS_TOKEN')
+	profile_data = json.loads(profile.text)
+
+
+	# Set limit
+	POST_MAX = 20
+	likes_sum = 0
+
+
+	# Find total likes count
+	for x in range(0,POST_MAX):
 		try:
-			captions = int((adata['data'][x]['likes']['count']))
-			sum = sum + captions
+			captions = int((media_data['data'][x]['likes']['count']))
+			likes_sum = likes_sum + captions
 		except IndexError:
-			captionserrormsg = "Not enough posts to give significant analysis, sorry!"
+			captionserrormsg = "Not enough posts to give significant analysis, try another profile?"
 		except KeyError:
-			userid = '25025320'
-			usererrormessage = " (User private, here's Instagram instead!)"
+			user_id = '25025320'
+			usererrormessage = " User private, here's Instagram instead!"
 
 
 
 	# Average likes
-	avg_likes = sum/20
+	allVars.avg_likes = likes_sum/POST_MAX
 
-	# Likes Count
-	lc = []
-	cc = []
-	fc = []
-	bff = []
-	geolat = []
-	geolong = []
-	geoname = []
 
-	for x in range(0,n):
+	# Find likes/comments/filters/tagged friends count per post
+	allVars.lc = []
+	allVars.cc = []
+	allVars.fc = []
+	allVars.bff = []
+
+	for x in range(0,POST_MAX):
 		try:
-			likes = int((adata['data'][x]['likes']['count']))
-			lc.append(likes)
-			commentcount  = int((adata['data'][x]['comments']['count']))
-			cc.append(commentcount)
-			filters  = (adata['data'][x]['filter'])
-			fc.append(filters)
-			bestfriends = (adata['data'][x]['users_in_photo'])
+			likes = int((media_data['data'][x]['likes']['count']))
+			allVars.lc.append(likes)
+
+			commentcount  = int((media_data['data'][x]['comments']['count']))
+			allVars.cc.append(commentcount)
+
+			filters  = (media_data['data'][x]['filter'])
+			allVars.fc.append(filters)
+
+			bestfriends = (media_data['data'][x]['users_in_photo'])
 			try:
 				for x in range(0,100):
 					bestfriendsindex = bestfriends[x]['user']['username']
-					bff.append(bestfriendsindex)
-
-
+					allVars.bff.append(bestfriendsindex)
 			except IndexError:
 				bfferrormsg = "whoops"
 
 		except IndexError:
-			captionserrormsg = "Not enough posts to give significant analysis, sorry!"
+			captionserrormsg = "Not enough posts to give significant analysis, try another account?"
+
 		except KeyError:
-			userid = '25025320'
+			user_id = '25025320'
 			usererrormessage = " (User private)"
 
-		# Geolocation
+
+
+
+	# Geolocation info
+	allVars.geolat = []
+	allVars.geolong = []
+	allVars.geoname = []
 
 	for i in range(0,21):
 		try:
-			lats = adata['data'][i]['location']['latitude']
-			longs = adata['data'][i]['location']['longitude']
-			names = adata['data'][i]['location']['name']
-			geolat.append(lats)
-			geolong.append(longs)
-			geoname.append(names.encode('ascii', 'ignore'))
+			lats = media_data['data'][i]['location']['latitude']
+			longs = media_data['data'][i]['location']['longitude']
+			names = media_data['data'][i]['location']['name']
+			allVars.geolat.append(lats)
+			allVars.geolong.append(longs)
+			allVars.geoname.append(names.encode('ascii', 'ignore'))
 
 		except TypeError:
 			errormsssg = ""
@@ -212,124 +164,126 @@ def getdata(userinput):
 		except IndexError:
 			errormsssg = ""
 
-	geolatlen = len(geolat)
+	allVars.geolatlen = len(allVars.geolat)
 
 
-	nodupfc = list(set(fc))
-	nodupbff = list(set(bff))
+	# Remove duplicates
+	allVars.nodupfc = list(set(allVars.fc))
+	allVars.nodupbff = list(set(allVars.bff))
 
-	fcc = []
-	bffval = []
 
-	for item in nodupfc:
-		fcc.append(fc.count(item))
+	# Filter/tag counts
+	allVars.fcc = []
+	allVars.bffval = []
 
-	for item in nodupbff:
-		bffval.append(int(bff.count(item)))
+	for item in allVars.nodupfc:
+		allVars.fcc.append(allVars.fc.count(item))
 
-	bffcount = len(nodupbff)
-	averagetagcount = round(float(bffcount/20.0),2)
+	for item in allVars.nodupbff:
+		allVars.bffval.append(int(allVars.bff.count(item)))
 
-	# Post Dates
-	pd = []
+	allVars.bffcount = len(allVars.nodupbff)
+	allVars.averagetagcount = round(float(allVars.bffcount/20.0),2)
 
-	for x in range(0,n):
+
+
+	# Get all post dates
+	allVars.pd = []
+
+	for x in range(0,POST_MAX):
 		try:
-			postdates = int((adata['data'][x]['created_time']))
+			postdates = int((media_data['data'][x]['created_time']))
 			postdatesvar = str(time.strftime("%D", time.localtime(postdates)))
-			pd.append(postdatesvar)
+			allVars.pd.append(postdatesvar)
 		except IndexError:
 			captionserrormsg = "Not enough posts to give significant analysis, sorry!"
 		except KeyError:
-			userid = '25025320'
+			user_id = '25025320'
 			usererrormessage = " (User private)"
 
 
 	# Day frequency
-	df = []
+	allVars.df = []
 
-	for x in range(0,n):
+	for x in range(0,POST_MAX):
 		try:
-			postdates = int((adata['data'][x]['created_time']))
+			postdates = int((media_data['data'][x]['created_time']))
 			postdatesvar = str(time.ctime(postdates)).split(" ")[0]
-			df.append(postdatesvar)
+			allVars.df.append(postdatesvar)
 		except IndexError:
 			captionserrormsg = "Not enough posts to give significant analysis, sorry!"
 		except KeyError:
-			userid = '25025320'
+			user_id = '25025320'
 			usererrormessage = " (User private)"
 
+	allVars.moncount = int(allVars.df.count("Mon"))
+	allVars.tuecount = int(allVars.df.count("Tue"))
+	allVars.wedcount = int(allVars.df.count("Wed"))
+	allVars.thucount = int(allVars.df.count("Thu"))
+	allVars.fricount = int(allVars.df.count("Fri"))
+	allVars.satcount = int(allVars.df.count("Sat"))
+	allVars.suncount = int(allVars.df.count("Sun"))
 
-
-	moncount = int(df.count("Mon"))
-	tuecount = int(df.count("Tue"))
-	wedcount = int(df.count("Wed"))
-	thucount = int(df.count("Thu"))
-	fricount = int(df.count("Fri"))
-	satcount = int(df.count("Sat"))
-	suncount = int(df.count("Sun"))
 
 
 	# Hour frequency
+	# First convert UNIX to readable date
+	allVars.hm = []
 
-	hm = []
-
-	# Convert UNIX to readable date
-	for x in range(0,n):
+	for x in range(0,POST_MAX):
 		try:
-			postdates = int((adata['data'][x]['created_time']))
+			postdates = int((media_data['data'][x]['created_time']))
 			postdatesvar = str(time.strftime("%H", time.localtime(postdates)))
-			hm.append(postdatesvar)
+			allVars.hm.append(postdatesvar)
 		except IndexError:
 			captionserrormsg = "Not enough posts to give significant analysis, sorry!"
 		except KeyError:
-			userid = '25025320'
+			user_id = '25025320'
 			usererrormessage = " (User private)"
 
 
 
-
-	hmc = []
-
 	# Get hour frequency labels
+	allVars.hmc = []
+
 	for x in range(0,24):
-		hmc.append(hm.count(str(x)))
+		allVars.hmc.append(allVars.hm.count(str(x)))
 
 
-	# Find variables
+
+	# Handle errors
 	try:
-		posts = (data['data']['counts']['media'])
+		posts = (profile_data['data']['counts']['media'])
 	except KeyError:
 		posts = 0
 		usererrormessage = " (User private)"
 	try:
-		followers = (data['data']['counts']['followed_by'])
+		allVars.followers = (profile_data['data']['counts']['followed_by'])
 	except KeyError:
-		followers = 0
+		allVars.followers = 0
 		usererrormessage = " (User private)"
-
 	try:
-		follows = (data['data']['counts']['follows'])
+		allVars.follows = (profile_data['data']['counts']['follows'])
 	except KeyError:
-		follows = 0
+		allVars.follows = 0
 		usererrormessage = " (User private)"
-
 	try:
-		ratio = round(float(followers)/float(follows),2)
+		allVars.ratio = round(float(allVars.followers)/float(allVars.follows),2)
 		zeroerrormsg = ""
-
 	except ZeroDivisionError:
-		ratio = "0"
-		zeroerrormsg = ", follows nobody"
+		allVars.ratio = "0"
+		zeroerrormsg = ". No follows"
+
 
 	# Max height of barchart
 	try:
-		max_height = max(lc)
-		max_heightfilter = max(fcc)
-		max_heightJS = int(max_height) + (int(max_height)*0.30)
-		max_heightfilterJS = int(max_heightfilter) + (int(max_heightfilter)*0.30)
+		allVars.max_height = max(allVars.lc)
+		allVars.max_heightfilter = max(allVars.fcc)
+		allVars.max_heightJS = int(allVars.max_height) + (int(allVars.max_height)*0.30)
+		allVars.max_heightfilterJS = int(allVars.max_heightfilter) + (int(allVars.max_heightfilter)*0.30)
 	except ValueError:
 		print ""
+
 
 
 @app.route("/chart")
@@ -337,20 +291,20 @@ def chart():
     userinput = request.args.get('username', '')
     getdata(userinput)
     labels = ["Followers","Follows"]
-    values = [followers,follows]
+    values = [allVars.followers,allVars.follows]
     colors = ["rgba(151,187,205,1)","rgba(238,98,98,1)"]
-    labels2 = pd
-    values2 = lc
-    values2_1 = cc
+    labels2 = allVars.pd
+    values2 = allVars.lc
+    values2_1 = allVars.cc
     labels3 = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-    values3 = [moncount,tuecount,wedcount,thucount,fricount,satcount,suncount]
+    values3 = [allVars.moncount, allVars.tuecount, allVars.wedcount, allVars.thucount, allVars.fricount, allVars.satcount, allVars.suncount]
     labels4 = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',]
-    values4 = hmc
-    labels5 = nodupfc
-    values5 = fcc
-    return render_template('chart.html', set=zip(values, labels, colors), userinput=userinput, avg_likes=avg_likes, posts=posts, followers=followers, follows=follows, values2=values2, values2_1=values2_1, labels2=labels2,
-    						 max_heightJS=max_heightJS, max_height2JS=max_height2JS, max_heightfilterJS=max_heightfilterJS,ratio=ratio, values3=values3, labels3=labels3, values4=values4, labels4=labels4, zeroerrormsg=zeroerrormsg,
-    						 usererrormessage=usererrormessage, values5=values5, labels5=labels5, nodupbff=nodupbff, bffcount=bffcount, averagetagcount=averagetagcount, geolong=geolong, geolat=geolat,geolatlen=geolatlen, geoname=geoname)
+    values4 = allVars.hmc
+    labels5 = allVars.nodupfc
+    values5 = allVars.fcc
+    return render_template('chart.html', set=zip(values, labels, colors), userinput=userinput, avg_likes=allVars.avg_likes, posts=allVars.posts, followers=allVars.followers, follows=allVars.follows, values2=values2, values2_1=values2_1, labels2=labels2,
+    						 max_heightJS=allVars.max_heightJS, max_height2JS=allVars.max_height2JS, max_heightfilterJS=allVars.max_heightfilterJS,ratio=allVars.ratio, values3=values3, labels3=labels3, values4=values4, labels4=labels4, zeroerrormsg=allVars.zeroerrormsg,
+    						 usererrormessage=allVars.usererrormessage, values5=values5, labels5=labels5, nodupbff=allVars.nodupbff, bffcount=allVars.bffcount, averagetagcount=allVars.averagetagcount, geolong=allVars.geolong, geolat=allVars.geolat,geolatlen=allVars.geolatlen, geoname=allVars.geoname)
 
 if __name__ == "__main__":
     app.debug = True
