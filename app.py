e029b3105d01601835a0507fb7915e00851b06ac
nikodraca <nikola.draca@live.ca>
2016-11-13 21:22:01 -0500

    basic template stuff

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def get_data(user_input):
 	# Get user info as dict, used to populate
 	user_info = user_request.json()['data']
 
+	# Get media data for last 20 posts
 	media_request = requests.get("https://api.instagram.com/v1/users/%s/media/recent/?access_token=%s&count=20" % (user_id, creds.ACCESS_TOKEN))
 
 	likes = []
@@ -40,21 +41,25 @@ def get_data(user_input):
 	tags = []
 	tag_positions = []
 
+	# Loop and get relevant data
 	for i in range(0, 20):
 		likes.append(media_request.json()['data'][i]['likes']['count'])
 		comments.append(media_request.json()['data'][i]['comments']['count'])
 		days.append(datetime.datetime.fromtimestamp(int(media_request.json()['data'][i]['created_time'])).strftime('%a'))
 		hours.append(datetime.datetime.fromtimestamp(int(media_request.json()['data'][i]['created_time'])).strftime('%H'))
 		filters.append(media_request.json()['data'][i]['filter'])
+
 		if not media_request.json()['data'][i]['location']:
 			pass
 		else:
 			locations.append([media_request.json()['data'][i]['location']['latitude'], media_request.json()['data'][i]['location']['longitude']])
+		
 		for x in range(0, len(media_request.json()['data'][i]['users_in_photo'])):
 			tags.append([media_request.json()['data'][i]['users_in_photo'][x]['user']['username'], media_request.json()['data'][i]['users_in_photo'][x]['user']['id']])
 			tag_positions.append([media_request.json()['data'][i]['users_in_photo'][x]['position']['x'], media_request.json()['data'][i]['users_in_photo'][x]['position']['y']])
 
 	all_data = {}
+	all_data['basic'] = user_info
 	all_data['likes'] = likes 
 	all_data['comments'] = comments
 	all_data['days'] = dict(Counter(days))
@@ -64,9 +69,9 @@ def get_data(user_input):
 	all_data['tags'] = tags
 	all_data['tag_positions'] = tag_positions
 
-	# return json.dumps(Counter(days))
+	# return json.dumps(all_data['likes'])
 
-	return json.dumps(all_data)
+	return render_template('chart.html', all_data=all_data)
 
 if __name__ == "__main__":
     app.debug = True
